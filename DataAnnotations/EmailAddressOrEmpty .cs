7f5cee68d39b9d5aced40f605fd97f91cc01707a
roygim <roygim@gmail.com>
2024-01-12 12:48:28 +0200

    .

---
+++ DataAnnotations/EmailAddressOrEmpty .cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TableBackend.DataAnnotations
+{
+    public class EmailAddressOrEmpty: ValidationAttribute
+    {
+        public const string DefaultErrorMessage = "אימייל לא תקין";
+        private EmailAddressAttribute _validator = new EmailAddressAttribute();
+
+        public EmailAddressOrEmpty() : base(DefaultErrorMessage)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (string.IsNullOrEmpty(value.ToString()))
+                return true;
+
+            return _validator.IsValid(value.ToString());
+        }
+    }
+}
--- Models/Table.cs
+++ Models/Table.cs
@@ -1,13 +1,28 @@
-﻿namespace TableBackend.Models
+﻿using System.ComponentModel.DataAnnotations;
+using TableBackend.DataAnnotations;
+
+namespace TableBackend.Models
 {
     public class TableObj
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "שדה חובה")]
+        [RegularExpression("^[0-9]+$", ErrorMessage = "מספרים בלבד")]
         public string UserId { get; set; }
+
+        [Required(ErrorMessage = "שדה חובה")]
         public string Name { get; set; }
+
+        [EmailAddressOrEmpty]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "שדה חובה")]
         public string Birthday { get; set; }
+
         public string Gender { get; set; }
+
+        [RegularExpression("^[0-9]+$", ErrorMessage = "מספרים בלבד")]
         public string Phone { get; set; }
     }
 }
